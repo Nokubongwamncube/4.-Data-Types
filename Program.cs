bd49622d7ceb19de92cba5fc68809df60f96b528
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-12 03:55:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declearation of a string, an interger, booleen and a decimal
+            string name = "Nokubongwa";
+            int age = 26;
+            bool isTall = true;
+            double weight = 76;
+            char initial = 'N';
+
+            //printing onto the screen
+
+            Console.WriteLine("My name is " + name + " I am " + age +  " it is " + isTall + " that i am Tall");
+            Console.WriteLine("My weight in Kg is " + weight + " and my initial is " + initial);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
